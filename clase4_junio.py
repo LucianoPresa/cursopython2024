e7aeb3735a18745db4966c68577e63d3306972c4
Luciano Presa <presa.luciano26@gmail.com>
2024-06-18 21:11:04 -0300

    actualizo mi proyecto en git

--- clase4_junio.py
+++ clase4_junio.py
@@ -4,3 +4,29 @@ print(1+9)
 sigo con comillas
 un reenglon mas'''
 print (25+25)
+
+print (30-5)
+print (30.5-5)
+dni = 35944440 #pones el dni y lo llamas con print abajo
+print (dni)
+# metodo de salida print()
+#metodo de entrada input()
+nombre = input("hola, escribi tu nombre:")
+print (nombre)
+
+a = 20
+b = 30
+print (a+b)
+
+
+e = 30
+f = int(input("cual es tu edad"))
+print (e+f)
+
+
+cadena_de_texto = "Python"
+anio_del_curso = "2024"
+print (cadena_de_texto + anio_del_curso)
+
+print (cadena_de_texto[2]) #imprime el caracter numero 2 arranca de 0 (0;1;2;3;4;5. etc)
+
